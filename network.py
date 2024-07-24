120287aa5d539d5e4923ed2b216708d0f0f93b92
Mahdi <zeinali.mahdy@gmail.com>
2024-07-25 01:12:49 +0330

    add network header

---
+++ network.py
@@ -0,0 +1,23 @@
+import torch
+from config import *
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 53 * 15, 128)
+        self.fc2 = nn.Linear(128, CLASSES_NUMBER)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(-1, 64 * 53 * 15)  # Flatten the tensor based on output size
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
