41e95b2d1cb69f4e037e7b220a6ae280ced175ad
Mahdi <zeinali.mahdy@gmail.com>
2024-07-25 02:31:46 +0330

    refactor data_loader

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,6 @@ from torch.utils.data import Dataset, DataLoader, random_split
 import torchvision.transforms as transforms
 from pycocotools.coco import COCO
 from PIL import Image
-import cv2
 import os
 
 import matplotlib.pyplot as plt
@@ -16,7 +15,7 @@ class COCODataset(Dataset):
     def __init__(self, root, annFile, transform=None):
         self.root = root
         self.coco = COCO(annFile)
-        self.ids = list(self.coco.imgToAnns.keys())
+        self.ids = list(sorted(self.coco.imgs.keys()))
         self.transform = transform
 
     def __len__(self):
@@ -27,67 +26,46 @@ class COCODataset(Dataset):
         ann_ids = self.coco.getAnnIds(imgIds=img_id)
         anns = self.coco.loadAnns(ann_ids)
         path = self.coco.loadImgs(img_id)[0]['file_name']
-
         img_path = os.path.join(self.root, path)
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = Image.open(img_path).convert('RGB')
+
+        num_objs = len(anns)
+        boxes = []
+        for i in range(num_objs):
+            xmin = anns[i]['bbox'][0]
+            ymin = anns[i]['bbox'][1]
+            xmax = xmin + anns[i]['bbox'][2]
+            ymax = ymin + anns[i]['bbox'][3]
+            boxes.append([xmin, ymin, xmax, ymax])
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        img_id = torch.tensor([img_id])
+        areas = torch.as_tensor([ann['area'] for ann in anns], dtype=torch.float32)
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = img_id
+        target["area"] = areas
+        target["iscrowd"] = iscrowd
 
         if self.transform is not None:
-            img = Image.fromarray(img)
             img = self.transform(img)
-        
-        return img, anns
 
-transform = transforms.Compose([
-    transforms.Resize((IMAGES_SIZE[1], IMAGES_SIZE[0])),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5])
-])
-
-
-coco_dataset = COCODataset(IMAGES, ANNOTATION, transform=transform)
-
-train_size = int(0.8 * len(coco_dataset))  # 80-20 split
-val_size = len(coco_dataset) - train_size
-train_dataset, val_dataset = random_split(coco_dataset, [train_size, val_size])
-
-
-def collate_fn(batch):
-    images, annotations = zip(*batch)
-
-    # Stack images into a single tensor
-    images = torch.stack(images)
-
-    # Pad annotations if needed
-    max_len = max(len(ann) for ann in annotations)
-    padded_annotations = []
-    for ann in annotations:
-        ann_padded = ann + [{}] * (max_len - len(ann))  # Pad with empty dicts
-        padded_annotations.append(ann_padded)
-
-    return images, padded_annotations
-
-
-TRAIN_SET = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
-VALID_SET = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
-
-
-# for i, (images, annotations) in enumerate(TRAIN_SET):
-#     print(f"Batch {i+1}")
-#     print("Images shape:", images.shape)  # Shape of the batch of images
-#     # print("Annotations:", annotations)  # Annotations for the batch
-#     if i == 1:  # Just show the first 2 batches for brevity
-#         break
-
-#=========================================================================================
+        return img, target
+    
 
 def draw_boxes(image, annotations):
     """Draw bounding boxes on the image."""
     draw = ImageDraw.Draw(image)
     for ann in annotations:
-        bbox = ann['bbox']
-
-        x, y, width, height = bbox
-        draw.rectangle([x, y, x + width, y + height], outline='red', width=2)
+        if 'boxes' in ann:
+            boxes = ann['boxes'].numpy()
+            for bbox in boxes:
+                x_min, y_min, x_max, y_max = bbox
+                draw.rectangle([x_min, y_min, x_max, y_max], outline='red', width=2)
     return image
 
 def show_images(dataset, num_images=5):
@@ -95,11 +73,41 @@ def show_images(dataset, num_images=5):
     for i in range(num_images):
         img, anns = dataset[i]
         img = TF.to_pil_image(img)
+        if isinstance(anns, dict):
+            anns = [anns]
         img = draw_boxes(img, anns)
         axes[i].imshow(img)
         axes[i].axis('off')
         axes[i].set_title(f'Image {i}')
     plt.show()
 
-# show_images(val_dataset, num_images=5)
-#=========================================================================================
+
+def collate_fn(batch):
+    images, targets = zip(*batch)
+    images = torch.stack(images)
+    return images, targets
+
+def create_dataset():
+    transform = transforms.Compose([
+        transforms.Resize((IMAGES_SIZE[1], IMAGES_SIZE[0])),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5]) 
+    ])
+
+    coco_dataset = COCODataset(IMAGES, ANNOTATION, transform=transform)
+    train_size = int(0.8 * len(coco_dataset))
+    val_size = len(coco_dataset) - train_size
+    train_dataset, val_dataset = random_split(coco_dataset, [train_size, val_size])
+
+    TRAIN_SET = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
+    VALID_SET = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
+    
+    # show_images(TRAIN_SET.dataset, num_images=10)
+
+    # for imgs, annotations in TRAIN_SET:
+    #     imgs = imgs.to("cuda") 
+    #     annotations = [{k: v.to("cuda") for k, v in t.items()} for t in annotations]
+    #     print(annotations)
+    
+    return TRAIN_SET, VALID_SET
