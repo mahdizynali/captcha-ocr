3b49ce714cf830832bf2f49dfed2546ca81c6114
Mahdizynali <zeinali.mahdy@gmail.com>
2025-04-04 11:03:55 +0330

    fix collate function to handle padds

--- data_loader.py
+++ data_loader.py
@@ -6,10 +6,10 @@ import torchvision.transforms as transforms
 from pycocotools.coco import COCO
 from PIL import Image
 import os
-
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
 from PIL import ImageDraw
+
 
 class COCODataset(Dataset):
     def __init__(self, root, annFile, transform=None):
@@ -29,102 +29,111 @@ class COCODataset(Dataset):
         img_path = os.path.join(self.root, path)
         img = Image.open(img_path).convert('RGB')
 
-        num_objs = len(anns)
-        boxes = []
-        labels = []
-        for i in range(num_objs):
-            xmin = anns[i]['bbox'][0]
-            ymin = anns[i]['bbox'][1]
-            xmax = xmin + anns[i]['bbox'][2]
-            ymax = ymin + anns[i]['bbox'][3]
+        boxes, labels = [], []
+        for ann in anns:
+            xmin, ymin, width, height = ann['bbox']
+            xmax, ymax = xmin + width, ymin + height
             boxes.append([xmin, ymin, xmax, ymax])
-            
-            category_id = anns[i]['category_id']
-            label = category_id_to_label[category_id]
+
+            category_id = ann['category_id']
+            label = category_id_to_label.get(category_id, 0)  # Default to 0 if not found
             labels.append(label)
 
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.as_tensor(labels, dtype=torch.int64)
+        boxes = torch.tensor(boxes, dtype=torch.float32) if boxes else torch.zeros((0, 4), dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64) if labels else torch.zeros((0,), dtype=torch.int64)
         img_id = torch.tensor([img_id])
-        
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["image_id"] = img_id
 
-        if self.transform is not None:
+        target = {"boxes": boxes, "labels": labels, "image_id": img_id}
+
+        if self.transform:
             img = self.transform(img)
 
         return img, target
-    
+
 
 def draw_boxes(image, annotations):
+
     draw = ImageDraw.Draw(image)
-    for ann in annotations:
-        if 'boxes' in ann:
-            boxes = ann['boxes'].numpy()
-            for bbox in boxes:
-                x_min, y_min, x_max, y_max = bbox
-                draw.rectangle([x_min, y_min, x_max, y_max], outline='red', width=2)
+    boxes = annotations.get("boxes", torch.empty((0, 4))).numpy()
+
+    for bbox in boxes:
+        x_min, y_min, x_max, y_max = bbox
+        draw.rectangle([x_min, y_min, x_max, y_max], outline='red', width=2)
+
     return image
 
+
 def show_images(dataset, num_images=5):
+
     fig, axes = plt.subplots(1, num_images, figsize=(15, 5))
     for i in range(num_images):
         img, anns = dataset[i]
-        img = TF.to_pil_image(img)
-        if isinstance(anns, dict):
-            anns = [anns]
+        img = TF.to_pil_image(img).convert("RGB")
+
         img = draw_boxes(img, anns)
         axes[i].imshow(img)
-        axes[i].axis('off')
-        axes[i].set_title(f'Image {i}')
+        axes[i].axis("off")
+        axes[i].set_title(f"Image {i}")
+
     plt.show()
 
 
-# def collate_fn(batch):
-#     images, targets = zip(*batch)
-#     images = torch.stack(images)
-#     return images, targets
-
 def collate_fn(batch):
+    """ Custom collate function to handle variable-sized tensors """
     images, targets = zip(*batch)
     images = torch.stack(images)
-    
-    boxes = [t['boxes'] for t in targets]
-    labels = [t['labels'] for t in targets]
-    max_boxes = max(len(b) for b in boxes)
-    
-    padded_boxes = [torch.cat([b, torch.zeros(max_boxes - len(b), 4)], dim=0) for b in boxes]
-    padded_labels = [torch.cat([l, torch.zeros(max_boxes - len(l))], dim=0) for l in labels]
-    
-    boxes_tensor = torch.stack(padded_boxes)
-    labels_tensor = torch.stack(padded_labels)
-    
-    targets = {'boxes': boxes_tensor, 'labels': labels_tensor}
+
+    max_boxes = 4  # Adjust based on dataset
+    max_labels = 4  # Adjust based on dataset
+
+    boxes, labels = [], []
+    for t in targets:
+        b = t["boxes"]
+        l = t["labels"]
+
+        # Truncate or pad bounding boxes
+        if len(b) > max_boxes:
+            b = b[:max_boxes]
+        else:
+            pad_b = torch.zeros((max_boxes - len(b), 4))
+            b = torch.cat([b, pad_b], dim=0)
+
+        # Truncate or pad labels
+        if len(l) > max_labels:
+            l = l[:max_labels]
+        else:
+            pad_l = torch.zeros((max_labels - len(l),), dtype=torch.int64)
+            l = torch.cat([l, pad_l], dim=0)
+
+        boxes.append(b)
+        labels.append(l)
+
+    targets = {
+        "boxes": torch.stack(boxes),
+        "labels": torch.stack(labels),
+    }
+
     return images, targets
 
 
 def create_dataset():
+
     transform = transforms.Compose([
         transforms.Resize((IMAGES_SIZE[1], IMAGES_SIZE[0])),
         transforms.Grayscale(num_output_channels=1),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5]) 
+        transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
-    coco_dataset = COCODataset(IMAGES, ANNOTATION, transform=transform)
-    train_size = int(0.8 * len(coco_dataset))
-    val_size = len(coco_dataset) - train_size
-    train_dataset, val_dataset = random_split(coco_dataset, [train_size, val_size])
+    dataset = COCODataset(IMAGES, ANNOTATION, transform=transform)
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
 
-    TRAIN_SET = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
-    VALID_SET = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
-    # show_images(TRAIN_SET.dataset, num_images=5)
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    # for imgs, annotations in TRAIN_SET:
-    #     imgs = imgs.to("cuda") 
-    #     annotations = [{k: v.to("cuda") for k, v in t.items()} for t in annotations]
-    #     print(annotations)
-    
-    return TRAIN_SET, VALID_SET
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+                              num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False,
+                            num_workers=WORKERS_NUMBER, collate_fn=collate_fn)
+
+    return train_loader, val_loader
